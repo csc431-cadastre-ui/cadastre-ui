89986b9573661f16dfaf4997c0fc1be3d63c0b13
Devin Michaels <d.michaels@miami.edu>
2018-04-15 17:44:09 -0400

    Added comments to leafletTest.js

--- leafletTest.js
+++ leafletTest.js
@@ -1,18 +1,22 @@
+// loads a Leaflet map and sets the center coordinate
 var mymap = L.map('mapid').setView([4.629159, -74.077034], 6);
 
+// loads actual images of the world
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
 }).addTo(mymap);
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
 var popup = L.popup();
 // for the polygon coordinates [lat, long]
+// append coordinate pairs to this, and when you click Complete Polygon, it
+// clears it and passes it to a geoJSON polygon
 var coordinates = [];
 
+// loads a geoJSON object array of preexisting polygons to display at start
 L.geoJSON(myObjects).addTo(mymap);
 
+//
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
@@ -25,13 +29,16 @@ function onMapClick(e) {
         fillOpacity: 0.5,
         radius: 20
     }).addTo(mymap);
+    // pushes this point to be added to the polygon
     coordinates.push([e.latlng.lat,e.latlng.lng])
 }
 
 mymap.on('click', onMapClick);
 
+//
 document.getElementById("createPolygon").addEventListener("click", function() {
+  // TODO want to add to geoJSON layer, not polygon layer
     var polygon = L.polygon(coordinates).addTo(mymap);
-    console.log(JSON.stringify(polygon.toGeoJSON()));
+    // console.log(JSON.stringify(polygon.toGeoJSON()));
     coordinates = [];
 }, false);
