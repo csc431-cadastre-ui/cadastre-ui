16415ce37f80958f80f66dad5004cddd53bd465e
BrandonSP <brandonsp6427@gmail.com>
2018-05-07 00:43:53 -0400

    Added UI error message

--- leafletTest.js
+++ leafletTest.js
@@ -58,6 +58,11 @@ map.on(L.Draw.Event.CREATED, function (event) {
     newPolygon = finishPolygon(layer.editing._poly.toGeoJSON());
     if (checkOverlap(newPolygon, existingPolygons)) {
       //ERROR MESSAGE
+	    console.log(newPolygon.geometry.coordinates[0]);
+	    console.log(newPolygon.geometry.coordinates[0][newPolygon.geometry.coordinates[0].length - 1]);
+      L.marker(newPolygon.geometry.coordinates[0][newPolygon.geometry.coordinates[0].length - 1]).addTo(map)
+	    .bindPopup('This polygon overlaps with an existing one')
+	    .openPopup();
       console.log('ERROR: OVERLAP DETECTED!!!!!!!!!!');
     } else {
       existingPolygons.push(newPolygon);
